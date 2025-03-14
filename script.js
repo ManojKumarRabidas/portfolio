404127eb14cf1ad502ceff0eb6618878b3c8c280
Manoj Kumar Rabidas <104958598+ManojKumarRabidas@users.noreply.github.com>
2025-03-14 17:54:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,56 @@
-/* document.getElementById("technical-blog-tab-menu").onclick((e)=>{
-   document.getElementById("technical-blog-tab-menu").add 
-}) */
 $(document).ready(function() {
-    $("#technical-blog-tab-menu").addClass("active-menu")
-    $("#technical-blog").show()
-    $("#case-study").hide()
-    $("#others").hide()
-
-    $("#collage-project-tab-menu").addClass("active-menu")
-    $("#collage-project").show()
-    $("#hackathon-project").hide()
-    $("#office-project").hide()
-    $("#non-academic-project").hide()
-    $("#other-project").hide()
-});
-$("#technical-blog-tab-menu").click(function(){
-    $("#technical-blog").show()
-    $("#technical-blog-tab-menu").addClass("active-menu")
-    $("#case-study").hide()
-    $("#case-study-tab-menu").removeClass("active-menu")
-    $("#others").hide()
-    $("#other-tab-menu").removeClass("active-menu")
-})
-$("#case-study-tab-menu").click(function(){
-    $("#technical-blog").hide()
-    $("#technical-blog-tab-menu").removeClass("active-menu")
-    $("#case-study").show()
-    $("#case-study-tab-menu").addClass("active-menu")
-    $("#others").hide()
-    $("#other-tab-menu").removeClass("active-menu")
-})
-$("#other-tab-menu").click(function(){
-    $("#technical-blog").hide()
-    $("#technical-blog-tab-menu").removeClass("active-menu")
-    $("#case-study").hide()
-    $("#case-study-tab-menu").removeClass("active-menu")
-    $("#others").show()
-    $("#other-tab-menu").addClass("active-menu")
-})
-$(".sidebar-icon").click(function(){
-    var sidebar = document.querySelector(".sidebar");
-    if(sidebar.style.width=="0vw"){
-        sidebar.style.width="60vw";
-    }
-    else{
-        sidebar.style.width="0vw";
-    }
-})
-
-function handelProject(type){
-    $("#non-academic-project-tab-menu").removeClass("active-menu")
-    $("#collage-project-tab-menu").removeClass("active-menu")
-    $("#hackathon-project-tab-menu").removeClass("active-menu")
-    $("#office-project-tab-menu").removeClass("active-menu")
-    $("#other-project-tab-menu").removeClass("active-menu")
-    $("#non-academic-project").hide()
-    $("#collage-project").hide()
-    $("#hackathon-project").hide()
-    $("#office-project").hide()
-    $("#other-project").hide()
-    if(type == "non-academic-project"){
-        $("#non-academic-project-tab-menu").addClass("active-menu")
-        $("#non-academic-project").show()
-    }else if(type == "collage-project"){
-        $("#collage-project-tab-menu").addClass("active-menu")
-        $("#collage-project").show()
-    }else if(type == "hackathon-project"){
-        $("#hackathon-project-tab-menu").addClass("active-menu")
-        $("#hackathon-project").show()
-    }else if(type == "office-project"){
-        $("#office-project-tab-menu").addClass("active-menu")
-        $("#office-project").show()
-    }else if(type == "other-project"){
-        $("#other-project-tab-menu").addClass("active-menu")
-        $("#other-project").show()
-    }
-}
+    // Initial tab setup
+    $("#technical-blog-tab-menu").addClass("active");
+    $("#technical-blog").addClass("active");
+    $("#collage-project-tab-menu").addClass("active");
+    $("#collage-project").addClass("active");
+  
+    // Tab switching for Blogs
+    $("#technical-blog-tab-menu").click(function() {
+      switchTab("technical-blog", ["case-study", "others"]);
+    });
+    $("#case-study-tab-menu").click(function() {
+      switchTab("case-study", ["technical-blog", "others"]);
+    });
+    $("#other-tab-menu").click(function() {
+      switchTab("others", ["technical-blog", "case-study"]);
+    });
+  
+    // Sidebar toggle
+    $("#sidebar-toggle, .close-sidebar").click(function() {
+      $(".sidebar").toggleClass("active");
+    });
+  
+    // GSAP Animations
+    gsap.from(".hero-content", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
+    gsap.from(".hero-image", { opacity: 0, x: 50, duration: 1, delay: 0.8 });
+  
+    gsap.utils.toArray(".section").forEach(section => {
+      gsap.from(section, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: section,
+          start: "top 80%",
+          toggleActions: "play none none none"
+        }
+      });
+    });
+  });
+  
+  function switchTab(activeTab, inactiveTabs) {
+    $(`#${activeTab}`).addClass("active").siblings().removeClass("active");
+    $(`#${activeTab}-tab-menu`).addClass("active").siblings().removeClass("active");
+    inactiveTabs.forEach(tab => $(`#${tab}`).removeClass("active"));
+  }
+  
+  function handelProject(type) {
+    const tabs = ["collage-project", "hackathon-project", "office-project", "non-academic-project", "other-project"];
+    tabs.forEach(tab => {
+      $(`#${tab}-tab-menu`).removeClass("active");
+      $(`#${tab}`).removeClass("active");
+    });
+    $(`#${type}-tab-menu`).addClass("active");
+    $(`#${type}`).addClass("active");
+  }
